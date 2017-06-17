712ada3eb88e41132b1402a031f8659d0f83c6e4
Julie Quinn <jmq2134@gmail.com>
2017-06-16 23:02:35 -0400

    Combined land and weather js

--- Assets/JS/land.js
+++ Assets/JS/land.js
@@ -1,3 +1,4 @@
+// Firebase call
 var config = {
     apiKey: "AIzaSyA21cA43E8JvGw-nZrN2ZgSMU6IJWjKiUQ",
     authDomain: "project1-work-space.firebaseapp.com",
@@ -11,29 +12,27 @@ firebase.initializeApp(config);
 var ref = firebase.database();
 
 
-
+// Geocode addresses from customer data table on index.html
 function geocodeAddress(location) {
-  
     var geocoder = new google.maps.Geocoder();
     geocoder.geocode({
-            'address': location.street1 + ' ' +location.city + ' ' +location.state + ' ' +location.zip
-     }
-, function(results, status) {
-      if (status == 'OK') {
-        window.mapInstance.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: window.mapInstance,
-            position: results[0].geometry.location
-        });     
-
-
-            } else {
-                alert("geocode of " + address + " failed:" + status);
-            }
-        });
+        'address': location.street1 + ' ' + location.city + ' ' + location.state + ' ' + location.zip
+    }, function(results, status) {
+        // Drop a pin on map for each geocoded address
+        if (status == 'OK') {
+            window.mapInstance.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: window.mapInstance,
+                position: results[0].geometry.location
+            });
+            // Error alert
+        } else {
+            alert("geocode of " + address + " failed:" + status);
+        }
+    });
 }
 
-
+// Location array
 ref.ref().on("value", function(snapshot) {
     var locations = snapshot.val();
     for (var key in locations) {
@@ -43,6 +42,7 @@ ref.ref().on("value", function(snapshot) {
     }
 })
 
+// Map settings
 function initAutocomplete() {
     var map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 41.505493, lng: -81.681290 },
@@ -50,6 +50,4 @@ function initAutocomplete() {
         mapTypeId: 'roadmap'
     });
     window.mapInstance = map;
-    
-
-    };
+};
--- Assets/JS/weather.js
+++ Assets/JS/weather.js
@@ -1,5 +1,61 @@
-// / FUNCTION TO RUN DARK SKY IFRAME WEATHER EMBED
+//////// MAP JS ////////
 
+// Firebase call
+var config = {
+    apiKey: "AIzaSyA21cA43E8JvGw-nZrN2ZgSMU6IJWjKiUQ",
+    authDomain: "project1-work-space.firebaseapp.com",
+    databaseURL: "https://project1-work-space.firebaseio.com",
+    projectId: "project1-work-space",
+    storageBucket: "project1-work-space.appspot.com",
+    messagingSenderId: "777786429739"
+};
+
+firebase.initializeApp(config);
+var ref = firebase.database();
+
+// Geocode addresses from customer data table on index.html
+function geocodeAddress(location) {
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({
+        'address': location.street1 + ' ' + location.city + ' ' + location.state + ' ' + location.zip
+    }, function(results, status) {
+        // Drop a pin on map for each geocoded address
+        if (status == 'OK') {
+            window.mapInstance.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: window.mapInstance,
+                position: results[0].geometry.location
+            });
+            // Error alert
+        } else {
+            alert("geocode of " + address + " failed:" + status);
+        }
+    });
+}
+
+// Location array
+ref.ref().on("value", function(snapshot) {
+    var locations = snapshot.val();
+    for (var key in locations) {
+        var location = locations[key];
+
+        geocodeAddress(location);
+    }
+})
+
+// Map settings
+function initAutocomplete(Lat, Long) {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: Lat, lng: Long },
+        zoom: 11,
+        mapTypeId: 'roadmap'
+    });
+    window.mapInstance = map;
+};
+
+//////// CALENDAR JS ////////
+
+/// FUNCTION TO RUN DARK SKY IFRAME WEATHER EMBED
 function weatherForecast() {
 
     // queryURL variables
@@ -27,6 +83,8 @@ function weatherForecast() {
         console.log(lat);
         console.log(long);
 
+        initAutocomplete(lat, long);
+
         // create weatherURL
         var weatherURL = "//forecast.io/embed/#lat=" + lat + "&lon=" + long + "&name=" + city;
         console.log(weatherURL);
@@ -34,9 +92,6 @@ function weatherForecast() {
         // Add weather URL to iframe
         $("#forecast_embed").attr("src", weatherURL);
     }); // close ajax request
-
-
-
 
 } // Close weather function
 
